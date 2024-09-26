1aa33c607446a4c062b98cf7d6898b420376a88f
Ben <benradley@cmail.carleton.ca>
2024-09-26 15:00:10 -0400

    adding entire project to git

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+         BuddyInfo buddy = new BuddyInfo("Tom", "613", "Carleton");
+         AddressBook addressBook = new AddressBook();
+    }
+}
